1f0f3f93c5a38c106cd2e73e1a201884be099889
albim72 <marcin.albiniak72@gmail.com>
2025-06-09 14:06:45 +0200

    Update budowniczy.cpp

--- DZIEN_1/budowniczy.cpp
+++ DZIEN_1/budowniczy.cpp
@@ -7,9 +7,22 @@ class Pizza {
         std::string sos;
         std::string dodatki;
         //ctrl + D -> duplikacja linii lub bloku kodu
+    
     public:
         virtual void showPizza() const {
             std::cout << "Pizza z " << ciasto << ", sosem " << sos << ", dodatkami: " << dodatki <<std::endl;
+        }
+
+        void set_ciasto(const std::string &ciasto) {
+            this->ciasto = ciasto;
+        }
+
+        void set_sos(const std::string &sos) {
+            this->sos = sos;
+        }
+
+        void set_dodatki(const std::string &dodatki) {
+            this->dodatki = dodatki;
         }
 };
 
@@ -23,5 +36,57 @@ class DeluxePizza: public Pizza {
     void showPizza() const override {
             Pizza::showPizza();
             std::cout << " + ekstra ser: " << extraSer << std::endl;
+        }
+};
+
+class PizzaBuilder {
+    protected:
+        std::string ciasto;
+        std::string sos;
+        std::string dodatki;
+    public:
+        virtual PizzaBuilder& setCiasto(const std::string& c) {
+            ciasto = c;
+            return *this;
+        }
+        virtual PizzaBuilder& setSos(const std::string& s) {
+            sos = s;
+            return *this;
+        }
+        virtual PizzaBuilder& setDodatki(const std::string& d) {
+            dodatki = d;
+            return *this;
+        }
+        virtual Pizza* build() {
+            if (ciasto.empty() && sos.empty()) {
+                throw std::runtime_error("PizzaBuilder: pizza musi mieć ciasto i sos!");
+            }
+            Pizza* p = new Pizza();
+            p->set_ciasto(ciasto);
+            p->set_sos(sos) ;
+            p->set_dodatki(dodatki);
+            return p;
+        }
+        virtual ~PizzaBuilder() = default;
+};
+
+class DeluxePizzaBuilder: public PizzaBuilder {
+    private:
+        std::string extraSer;
+    public:
+        DeluxePizzaBuilder& setExtraSer(const std::string& ser) {
+            extraSer = ser;
+            return *this;
+        }
+        Pizza* build() override {
+            if (extraSer.empty()) {
+                throw std::runtime_error("Pizza Deluxe musi mieć dodatkowy ser!");
+            }
+            DeluxePizza* p = new DeluxePizza();
+            p->set_ciasto(ciasto);
+            p->set_sos(sos);
+            p->set_dodatki(dodatki);
+            p->setExtraSer(extraSer);
+            return p;
         }
 };
