c3ff47475a023552f279d95cae1f197fa086e412
albim72 <marcin.albiniak72@gmail.com>
2025-06-09 12:19:53 +0200

    Create singleton.cpp

---
+++ DZIEN_1/singleton.cpp
@@ -0,0 +1,42 @@
+#include <iostream> //do wypisywana na ekran
+#include <mutex> //do synchronizacjii wielowoątkowej
+
+class Singleton {
+
+private:
+    static Singleton* instance; //wskaźnik do jednej instancji singletona
+    static std::mutex mutex; //bezpieczny dotęp do wielu wątków
+
+    //konstruktor prywatny
+    Singleton() {
+        std::cout << "Singleton constructor" << std::endl;
+    }
+    //zabronienie kopiowania
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+public:
+    static Singleton* getInstance() {
+        std::lock_guard<std::mutex> lock(mutex);
+        if (instance == nullptr) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    void doSomething() {
+        std::cout << "Singleton doSomething" << std::endl;
+    }
+};
+
+//inicjalizacja pól statycznych
+Singleton* Singleton::instance = nullptr;
+std::mutex Singleton::mutex;
+int main() {
+    Singleton* s1 = Singleton::getInstance();
+    s1->doSomething();
+
+    Singleton* s2 = Singleton::getInstance();
+    s2->doSomething();
+    return 0;
+}
