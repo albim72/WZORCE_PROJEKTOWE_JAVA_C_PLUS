efc9ca15f97fc44534ae4038814a50a2ef260331
albim72 <marcin.albiniak72@gmail.com>
2025-06-10 10:12:21 +0200

    Update Main.java

--- fasadawzorzec/Main.java
+++ fasadawzorzec/Main.java
@@ -1,1 +1,7 @@
 
+public class Main {
+    public static void main(String[] args) {
+        TravelFasade travelFasade = new TravelFasade();
+        travelFasade.bookTrip("Paryż","Paryż","Lotnisko Chopina");
+    }
+}
