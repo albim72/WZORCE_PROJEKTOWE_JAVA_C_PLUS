08385c56c7a94d09e467cb9bf22ee67fddac52ef
albim72 <marcin.albiniak72@gmail.com>
2025-06-10 08:55:12 +0200

    Create CustomerCSV.java

---
+++ DZIEN_2/adapter_formaty_danych/CustomerCSV.java
@@ -0,0 +1,10 @@
+public class CustomerCSV {
+    private String csvLine;
+
+    public CustomerCSV(String csvLine) {
+        this.csvLine = csvLine;
+    }
+    public String getCsv() {
+        return csvLine;
+    }
+}
