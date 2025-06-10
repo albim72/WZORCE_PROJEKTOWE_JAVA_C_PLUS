280d7168df4b7a98b54e7b486aec6b3090e1d068
albim72 <marcin.albiniak72@gmail.com>
2025-06-10 10:48:00 +0200

    Update Main.java

--- DZIEN_2/dekorator_kawiarnia/Main.java
+++ DZIEN_2/dekorator_kawiarnia/Main.java
@@ -1,1 +1,11 @@
 
+public class Main {
+    public static void main(String[] args) {
+        Napoj napoj = new Espresso();
+        napoj = new Mleko(napoj);
+        napoj = new Czekolada(napoj);
+
+        System.out.println("Zamówienie: " + napoj.pobierzOpis());
+        System.out.println("Cena: " + String.format("%.2f",napoj.koszt()) + "zł");
+    }
+}
