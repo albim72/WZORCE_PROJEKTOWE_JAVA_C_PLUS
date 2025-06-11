ca2273a6b1282d1b6ec2a0c32fdc7922b9a227f8
albim72 <marcin.albiniak72@gmail.com>
2025-06-11 09:09:14 +0200

    Create PlusExpression.java

---
+++ DZIEN_3/INTERPRETER_EXPR/PlusExpression.java
@@ -0,0 +1,13 @@
+public class PlusExpression implements Expression {
+    private Expression left,right;
+
+    public PlusExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret() {
+        return left.interpret() + right.interpret();
+    }
+}
