676337271c210bb83ee49e459a1e344c1fed31f8
albim72 <marcin.albiniak72@gmail.com>
2025-06-11 09:10:44 +0200

    Create MinusExpression.java

---
+++ DZIEN_3/INTERPRETER_EXPR/MinusExpression.java
@@ -0,0 +1,13 @@
+public class MinusExpression implements  Expression {
+    private  Expression left,right;
+
+    public MinusExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret() {
+        return left.interpret() - right.interpret();
+    }
+}
